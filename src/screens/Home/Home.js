7aa53fec11a39b22c9560eeb2d5290bcb6dbc75e
sulthanqintara <sulthanqintara@gmail.com>
2021-10-15 17:38:47 +0700

    add home, add vector icon on build.gradle

---
+++ src/screens/Home/Home.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import {View, Text, Image, Pressable} from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+import styles from './Style';
+import profilePlaceHolder from '../../assets/img/profile.png';
+import SpotifyIcon from '../../assets/img/spotify.png';
+
+const Home = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={profilePlaceHolder} style={styles.profilePic} />
+        <View style={styles.headerTextContainer}>
+          <Text style={[styles.nunito400, styles.balanceTxt]}>Balance</Text>
+          <Text style={[styles.nunito700, styles.balanceAmount]}>
+            Rp 120.000
+          </Text>
+        </View>
+        <Pressable>
+          <Ionicons name="notifications-outline" size={25} color="white" />
+        </Pressable>
+      </View>
+      <View>
+        <View style={styles.topSection}>
+          <View style={styles.flexRow}>
+            <Pressable style={[styles.topButton, styles.flexRow]}>
+              <Ionicons name="arrow-up" size={28} color="#608DE2" />
+              <Text style={[styles.nunito700, styles.topButtonTxt]}>
+                Transfer
+              </Text>
+            </Pressable>
+            <Pressable style={[styles.topButton, styles.flexRow]}>
+              <Ionicons name="add" size={36} color="#608DE2" />
+              <Text style={[styles.nunito700, styles.topButtonTxt]}>
+                Top Up
+              </Text>
+            </Pressable>
+          </View>
+          <View style={[styles.flexRow, styles.transactionTitleContainer]}>
+            <Text style={[styles.transactionHistoryTxt, styles.nunito700]}>
+              Transaction History
+            </Text>
+            <Pressable>
+              <Text style={[styles.seeAllTxt, styles.nunito400]}>See all</Text>
+            </Pressable>
+          </View>
+        </View>
+        <View style={[styles.homeCardContainer, styles.flexRow]}>
+          <View style={styles.cardPic}>
+            <Image source={SpotifyIcon} style={styles.cardPicIcon} />
+          </View>
+          <View>
+            <Text style={[styles.nunito700, styles.cardTitle]}>Spotify</Text>
+            <Text>Subscription</Text>
+          </View>
+          <Text style={[styles.cardNominal, styles.nunito700, styles.redText]}>
+            -Rp149000
+          </Text>
+        </View>
+        <View style={[styles.homeCardContainer, styles.flexRow]}>
+          <View style={styles.cardPic}>
+            <Image source={profilePlaceHolder} style={styles.cardPicImage} />
+          </View>
+          <View>
+            <Text style={[styles.nunito700, styles.cardTitle]}>Bobi Sammy</Text>
+            <Text>Transfer</Text>
+          </View>
+          <Text
+            style={[styles.cardNominal, styles.nunito700, styles.greenText]}>
+            +Rp1.150.000
+          </Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default Home;
---
+++ src/screens/Home/Style.js
@@ -0,0 +1,103 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  container: {flex: 1, backgroundColor: '#FAFCFF'},
+  header: {
+    height: 156,
+    backgroundColor: '#6379F4',
+    borderBottomEndRadius: 15,
+    borderBottomStartRadius: 15,
+    alignItems: 'center',
+    paddingHorizontal: 16,
+    flexDirection: 'row',
+  },
+  profilePic: {
+    width: 52,
+    height: 52,
+    borderRadius: 10,
+  },
+  headerTextContainer: {
+    marginLeft: 20,
+    marginRight: 'auto',
+  },
+  nunito700: {
+    fontFamily: 'NunitoSans-Bold',
+    fontWeight: '700',
+  },
+  nunito400: {fontFamily: 'NunitoSans-Regular'},
+  balanceTxt: {
+    color: 'white',
+  },
+  balanceAmount: {
+    fontSize: 24,
+    color: 'white',
+    marginTop: 10,
+  },
+  topSection: {
+    paddingHorizontal: 16,
+    paddingVertical: 25,
+  },
+  flexRow: {flexDirection: 'row'},
+  topButton: {
+    width: 162,
+    height: 57,
+    backgroundColor: '#EAEDFF',
+    borderRadius: 10,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  topButtonTxt: {fontSize: 18, marginLeft: 10},
+  transactionTitleContainer: {
+    marginTop: 40,
+    alignItems: 'center',
+  },
+  transactionHistoryTxt: {
+    fontSize: 18,
+    marginRight: 'auto',
+  },
+  seeAllTxt: {
+    color: '#6379F4',
+  },
+  homeCardContainer: {
+    backgroundColor: 'white',
+    borderRadius: 10,
+    width: 'auto',
+    height: 96,
+    elevation: 5,
+    paddingHorizontal: 16,
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  cardPic: {
+    width: 56,
+    height: 56,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#EBEEF2',
+    borderRadius: 10,
+    marginRight: 16,
+  },
+  cardPicIcon: {
+    width: 36,
+    height: 36,
+  },
+  cardTitle: {
+    fontSize: 16,
+    marginBottom: 10,
+  },
+  cardNominal: {
+    marginLeft: 'auto',
+    fontSize: 18,
+  },
+  redText: {
+    color: '#FF5B37',
+  },
+  greenText: {color: '#1EC15F'},
+  cardPicImage: {
+    width: 56,
+    height: 56,
+    borderRadius: 10,
+  },
+});
